e138da1ba0f318aeef6d4405f392e3904e589d30
Ivonne Baca Flores <BacaI@DIRSA.DIRSAMEXICO.COM>
2023-06-22 16:57:04 -0600

    Actualizacion 220626

--- src/app/auth/inventory/transfer/transfer-store/transfer-store.component.ts
+++ src/app/auth/inventory/transfer/transfer-store/transfer-store.component.ts
@@ -1,4 +1,4 @@
-/*import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserservicesService } from 'src/app/auth/service/userservices.service';
@@ -178,4 +178,4 @@ export class TransferStoreComponent {
 
 
 
-}*/
+}
